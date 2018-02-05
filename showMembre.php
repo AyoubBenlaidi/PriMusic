f179e52b20d1014a2a2fa29dee589f9e3051a191
Malamercery <malamercery@hotmail.fr>
2018-02-05 16:07:14 +0100

    Add files via upload

--- showMembre.php
+++ showMembre.php
@@ -18,83 +18,83 @@
 
 	<?php
 	
-	for(  $i=0 ;$i< $_SESSION['nb_adh'] ; $i++){
-		if ($_SESSION['adh_nom'.$i] != NULL) { 
-			echo("<tr>") ;
-				echo("<td>".$_SESSION['adh_nom'.$i]."</td>"); 
-				echo("<td>".$_SESSION['adh_prenom'.$i] ."</td>");
-				$verif = 0;
-				for($j = 0, $size = count($instrArray); $j < $size; $j++) {
-					if($instrArray[$j][0] == $_SESSION['adh_instr1'.$i] ){
-						echo("<td>".$instrArray[$j][1]."</td>");
-						$verif = 1;
-					}
+	$familleDataArray = databaseQuery('SELECT adh_nom, adh_prenom, adh_instr1, adh_prof1, adh_instr2, adh_prof2, adh_atelier1, adh_atelier2, adh_formation FROM adherent WHERE adh_fml=\'' . $_SESSION['fml_id'] .'\'');
+	
+	foreach($familleDataArray as $adh_nb => $adherent){
+		echo("<tr>") ;
+			echo("<td>".$adherent[0]."</td>");	//adh_nom
+			echo("<td>".$adherent[1] ."</td>");	//adh_prenom
+			$verif = 0;
+			for($j = 0, $size = count($instrArray); $j < $size; $j++) {
+				if($instrArray[$j][0] == $adherent[2] ){	//adh_instr1
+					echo("<td>".$instrArray[$j][1]."</td>");
+					$verif = 1;
 				}
-				if($verif == 0){
-					echo "<td></td>";
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($profArray); $j < $size; $j++) {
+				if($profArray[$j][0] == $adherent[3] ){	//adh_prof1
+					echo("<td>".$profArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				$verif = 0;
-				for($j = 0, $size = count($instrArray); $j < $size; $j++) {
-					if($instrArray[$j][0] == $_SESSION['adh_instr2'.$i] ){
-						echo("<td>".$instrArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($instrArray); $j < $size; $j++) {
+				if($instrArray[$j][0] == $adherent[4] ){	//adh_instr2
+					echo("<td>".$instrArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				if($verif == 0){
-					echo "<td></td>";
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($profArray); $j < $size; $j++) {
+				if($profArray[$j][0] == $adherent[5] ){	//adh_prof2
+					echo("<td>".$profArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				$verif = 0;
-				for($j = 0, $size = count($profArray); $j < $size; $j++) {
-					if($profArray[$j][0] == $_SESSION['adh_prof1'.$i] ){
-						echo("<td>".$profArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
+				if($atelierArray[$j][0] == $adherent[6] ){	//adh_atelier1
+					echo("<td>".$atelierArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				if($verif == 0){
-					echo "<td></td>";
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
+				if($atelierArray[$j][0] == $adherent[7] ){	//adh_atelier2
+					echo("<td>".$atelierArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				$verif = 0;
-				for($j = 0, $size = count($profArray); $j < $size; $j++) {
-					if($profArray[$j][0] == $_SESSION['adh_prof2'.$i] ){
-						echo(" <td>".$profArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+			$verif = 0;
+			for($j = 0, $size = count($formationArray); $j < $size; $j++) {
+				if($formationArray[$j][0] == $adherent[8] ){	//adh_formation
+					echo("<td>".$formationArray[$j][1]."</td>"); 
+					$verif = 1;
 				}
-				if($verif == 0){
-					echo "<td></td>";
-				}
-				$verif = 0;
-				for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
-					if($atelierArray[$j][0] == $_SESSION['adh_atelier1'.$i] ){
-						echo("<td>".$atelierArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
-				}
-				if($verif == 0){
-					echo "<td></td>";
-				}
-				$verif = 0;
-				for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
-					if($atelierArray[$j][0] == $_SESSION['adh_atelier2'.$i] ){
-						echo("<td>".$atelierArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
-				}
-				if($verif == 0){
-					echo "<td></td>";
-				}
-				$verif = 0;
-				for($j = 0, $size = count($formationArray); $j < $size; $j++) {
-					if($formationArray[$j][0] == $_SESSION['adh_formation'.$i] ){
-						echo("<td>".$formationArray[$j][1]."</td>"); 
-						$verif = 1;
-					}
-				}
-				if($verif == 0){
-					echo "<td></td>";
-				}
-			echo("</tr>") ;
-		}
+			}
+			if($verif == 0){
+				echo "<td></td>";
+			}
+		echo("</tr>") ;
 	}
 	?>
 	</tbody>
