bb1966da2b9cd24b75a25ca74955d0274a7bd67f
Malamercery <malamercery@hotmail.fr>
2018-02-05 14:47:37 +0100

    Add files via upload

--- formulaireAdherent.php
+++ formulaireAdherent.php
@@ -2,17 +2,16 @@
 <html>
 	<head>
 		<title>Ajouter Adhérents</title>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+		
+		<link rel="stylesheet" href="./ressources/bootstrap/css/bootstrap.min.css">
+		<script src="./ressources/jquery-3.2.1.min.js"></script>
+		<script src="./ressources/bootstrap/js/bootstrap.min.js"></script>
+		<link rel="stylesheet" href="./ressources/font-awesome-4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="./ressources/MDL/material.min.css">
+		<script defer src="./ressources/MDL/material.min.js"></script>
 
-		<script src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4=" crossorigin="anonymous"></script>
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-		<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-		<link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.grey-indigo.min.css">
-		<script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
 		<meta charset="utf-8" />
-
+		
 		<script type="text/javascript" src="scriptAdh.js"></script>
 	</head>
 
--- showMembre.php
+++ showMembre.php
@@ -1,108 +1,101 @@
 <table class="table table-bordered table-striped " >
 
-<tbody>
-<?php 
-/*
-  for(  $i=0 ;$i< $_SESSION['nb_adh'] ; $i++){
-	if ($_SESSION['adh_nom'.$i] != null) { 
+	<thead>
+		<tr>
+			<td>Nom</td>
+			<td>Prénom</td>
+			<td>Instrument 1</td>
+			<td>Instrument 2</td>
+			<td>Professeur 1</td>
+			<td>Professeur 2</td>
+			<td>Atelier 1</td>
+			<td>Atelier 2</td>
+			<td>Formation</td>
+		</tr>
+	</thead>
 
-$adh_prenom=$_SESSION['adh_prenom'.$i];
-$adh_nom=$_SESSION['adh_nom'.$i];
-$instr1 = databaseQuery('SELECT adh_instr1 FROM adherent WHERE adh_prenom = '.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$prof1 =databaseQuery('SELECT adh_prof1 FROM adherent WHERE adh_prenom = '.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$instr2=databaseQuery('SELECT adh_instr2 FROM adherent WHERE adh_prenom ='.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$prof2=databaseQuery('SELECT adh_prof2 FROM adherent WHERE adh_prenom = '.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$atl1=databaseQuery('SELECT adh_atelier1 FROM adherent WHERE adh_prenom = '.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$atl2=databaseQuery('SELECT adh_atelier2 FROM adherent WHERE adh_prenom ='.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
-$formation=databaseQuery('SELECT adh_formation FROM adherent WHERE adh_prenom ='.$adh_prenom.' AND adh_nom = '.$adh_nom.';' );
+	<tbody>
 
-
-echo("*/
-?>
-
-
-
-<?php
-
- for(  $i=0 ;$i< $_SESSION['nb_adh'] ; $i++){
-	if ($_SESSION['adh_nom'.$i] != NULL) { 
-        echo("<tr>") ;
-        echo(
-            " <td> Nom :".     $_SESSION['adh_nom'.$i]  ."</td>"
-         
-         );  echo(
-            " <td> Prenom :".     $_SESSION['adh_prenom'.$i] ."</td>" 
-         ); 
-
-
-        for($j = 0, $size = count($instrArray); $j < $size; $j++) {
-          if($instrArray[$j][0] == $_SESSION['adh_instr1'.$i] ){
-            
-               
-                echo(
-                
-               " <td> Instrument 1 :".     $instrArray[$j][1]  ."</td>"
-			
-            ); 
-          }}
-          
-        for($j = 0, $size = count($instrArray); $j < $size; $j++) {
-            if($instrArray[$j][0] == $_SESSION['adh_instr2'.$i] ){
-                
-              echo(
-                 " <td> Instrument 2 :".     $instrArray[$j][1]  ."</td>"
-              
-              ); 
-            }}
-            for($j = 0, $size = count($profArray); $j < $size; $j++) {
-                if($profArray[$j][0] == $_SESSION['adh_prof1'.$i] ){
-                  
-                  echo(
-                     " <td> Professeur 1 :".     $profArray[$j][1]  ."</td>"
-                  
-                  ); 
-                }}
-        
-                for($j = 0, $size = count($profArray); $j < $size; $j++) {
-                    if($profArray[$j][0] == $_SESSION['adh_prof2'.$i] ){
-                       
-                      echo(
-                          
-                         " <td> Professeur 2 :".     $profArray[$j][1]  ."</td>"
-                      
-                      ); 
-                    }}
-                    for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
-                        if($atelierArray[$j][0] == $_SESSION['adh_atelier1'.$i] ){
-                          
-                          echo(
-                             " <td> Atelier 1 :".     $atelierArray[$j][1]  ."</td>"
-                          
-                          ); 
-                        }}
-                        for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
-                            if($atelierArray[$j][0] == $_SESSION['adh_atelier2'.$i] ){
-                              
-                              echo(
-                                 " <td> Atelier 2 :".     $atelierArray[$j][1]  ."</td>"
-                              
-                              ); 
-                            }}
-        
-                            for($j = 0, $size = count($formationArray); $j < $size; $j++) {
-                                if($formationArray[$j][0] == $_SESSION['adh_formation'.$i] ){
-                                
-                                  echo(
-                                     " <td>Formation :".     $formationArray[$j][1]  ."</td>"
-                                  
-                                  ); 
-                                }}
-            
-        
-        
-        }}
-        ?>
- 
-</tr>
+	<?php
+	
+	for(  $i=0 ;$i< $_SESSION['nb_adh'] ; $i++){
+		if ($_SESSION['adh_nom'.$i] != NULL) { 
+			echo("<tr>") ;
+				echo("<td>".$_SESSION['adh_nom'.$i]."</td>"); 
+				echo("<td>".$_SESSION['adh_prenom'.$i] ."</td>");
+				$verif = 0;
+				for($j = 0, $size = count($instrArray); $j < $size; $j++) {
+					if($instrArray[$j][0] == $_SESSION['adh_instr1'.$i] ){
+						echo("<td>".$instrArray[$j][1]."</td>");
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($instrArray); $j < $size; $j++) {
+					if($instrArray[$j][0] == $_SESSION['adh_instr2'.$i] ){
+						echo("<td>".$instrArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($profArray); $j < $size; $j++) {
+					if($profArray[$j][0] == $_SESSION['adh_prof1'.$i] ){
+						echo("<td>".$profArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($profArray); $j < $size; $j++) {
+					if($profArray[$j][0] == $_SESSION['adh_prof2'.$i] ){
+						echo(" <td>".$profArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
+					if($atelierArray[$j][0] == $_SESSION['adh_atelier1'.$i] ){
+						echo("<td>".$atelierArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($atelierArray); $j < $size; $j++) {
+					if($atelierArray[$j][0] == $_SESSION['adh_atelier2'.$i] ){
+						echo("<td>".$atelierArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+				$verif = 0;
+				for($j = 0, $size = count($formationArray); $j < $size; $j++) {
+					if($formationArray[$j][0] == $_SESSION['adh_formation'.$i] ){
+						echo("<td>".$formationArray[$j][1]."</td>"); 
+						$verif = 1;
+					}
+				}
+				if($verif == 0){
+					echo "<td></td>";
+				}
+			echo("</tr>") ;
+		}
+	}
+	?>
 	</tbody>
 </table>
